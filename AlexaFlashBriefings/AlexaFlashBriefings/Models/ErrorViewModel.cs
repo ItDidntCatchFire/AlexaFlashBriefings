030b5f23b89ef5d169f783f895fd4c4e68ae1fc2
ItDidntCatchFire <deaddanster@googlemail.com>
2018-07-20 01:57:33 +0100

    Inital Commit
    Started the project

---
+++ AlexaFlashBriefings/AlexaFlashBriefings/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AlexaFlashBriefings.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
