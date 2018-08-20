57ed20db51b43a8a8076ce837845e4f7e3805c24
Dan <deaddanster@googlemail.com>
2018-08-20 22:41:59 +0100

    Tried to Make it work on Linux

--- AlexaFlashBriefings/AlexaFlashBriefings/Startup.cs
+++ AlexaFlashBriefings/AlexaFlashBriefings/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -39,7 +40,16 @@ namespace AlexaFlashBriefings
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+
+                app.UseForwardedHeaders(new ForwardedHeadersOptions
+                {
+                    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+                });
+
+                app.UseAuthentication();
             }
+
+
 
             app.UseStaticFiles();
 
